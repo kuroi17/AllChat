2b46087ae3451cb320cdd7b646ff3552f6fa093e
kuroi017 <painkira518@gmail.com>
2026-02-21 21:43:24 +0800

    tailwind React setUp

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
